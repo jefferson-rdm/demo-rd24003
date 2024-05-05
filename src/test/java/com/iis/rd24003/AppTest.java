74d5d31cc05613aa3a7e8bb2627528423bbaf7a1
Jefferson Ramos <97738626+jefferson-rdm@users.noreply.github.com>
2024-05-04 19:14:43 -0600

    initial commit

---
+++ src/test/java/com/iis/rd24003/AppTest.java
@@ -0,0 +1,23 @@
+package com.iis.rd24003;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Rigourous Test :-)
+     */
+    // @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
